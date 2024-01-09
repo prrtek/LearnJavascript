e4688886c51a2d975665a5b787cf8c5f880df1d6
prrtek <prrtek@outlook.com>
2024-01-09 18:31:38 +0530

    Tricky Added

---
+++ Basics_01/04_tricky.js
@@ -0,0 +1,8 @@
+let a = 12;
+let b = "123";
+let c = b + a + a;
+
+console.log(typeof c);
+console.log(c);
+
+//Just simple start from left to right if any of the operand is string then it will concatenate and type is string
