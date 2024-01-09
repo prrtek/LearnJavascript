3bbe918155da406e766e07aec1d08c01a3637b0d
prrtek <prrtek@outlook.com>
2024-01-09 18:38:17 +0530

    05 comaprison added

---
+++ Basics_01/05_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2); //use this becouse it also checks for data type and doesnt convert the values
