5b13e9fb3f7cb62c7b26a3c4d0378ccfa01069a1
prrtek <prrtek@outlook.com>
2024-01-10 12:38:12 +0530

    Arrays

---
+++ Basics_02/01_array.js
@@ -0,0 +1,37 @@
+const my = [1, 2, 3, 4, 5, 6, "Prateek"];
+const my2 = new Array(1, 2, 3);
+
+// console.log(my);
+// console.log(my2);
+
+// my.push(my2);
+// console.log(my);
+// console.log(my2);
+
+// console.log(my[7][0]);
+// console.log(typeof my);
+
+// const my3 = my2;
+// my3[0] = 0;
+// console.log(my3);
+// console.log(my2);
+//dono change honge kyuki sma echeez pass hogi refrence pointerr alaghai lekin cheez same
+
+// my.push(7, 8, 9, "");
+
+// console.log(my);
+
+// const rem = my.pop();
+// console.log(rem);
+
+// const rem2 = my.pop();
+// console.log(rem2);
+
+// my.push(9);
+// console.log(my);
+
+//unshift() adds to first shift removes from first
+
+console.log(my.join("")); //concatenates the value of array you can se anny seprator
+
+//learn about slice and splice
