734756cb66085c56d2319fd2bc333a50d6e1bbd2
prrtek <prrtek@outlook.com>
2024-01-22 22:34:41 +0530

    Digital clock

--- Project_06/bmi.js
+++ Project_06/bmi.js
@@ -7,4 +7,14 @@ form.addEventListener("submit", (e) => {
   const result = document.querySelector("#results");
   const bmi = weight / ((height * height) / 10000).toFixed(2);
   result.innerHTML = `${bmi}`;
+
+  const sp = document.querySelector(".res");
+
+  if (bmi > 24.9) {
+    sp.innerHTML = "Overweight";
+  } else if (bmi > 18.6 && bmi <= 24.9) {
+    sp.innerHTML = "Normal";
+  } else {
+    sp.innerHTML = "Underweight";
+  }
 });
